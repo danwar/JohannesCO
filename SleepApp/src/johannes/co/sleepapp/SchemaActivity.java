eaf894c9bd162025b86a00040eb70d1c989c8f24
Daniel Warme <daniel.warme@gmail.com>
2012-04-19 11:38:56 +0200

    fixade array och tog bort ondig kod

--- SleepApp/src/johannes/co/sleepapp/SchemaActivity.java
+++ SleepApp/src/johannes/co/sleepapp/SchemaActivity.java
@@ -15,7 +15,8 @@ import android.widget.TimePicker;
 
 public class SchemaActivity extends Activity {
 	
-    private TextView mTimeDisplay, mTimeDisplay1, mTimeDisplay2, mTimeDisplay3, mTimeDisplay4, mTimeDisplay5;
+	private TextView[] mTimeDisplay = new TextView[6];
+//    private TextView mTimeDisplay, mTimeDisplay1, mTimeDisplay2, mTimeDisplay3, mTimeDisplay4, mTimeDisplay5;
     private Button mPickTime;
 
     private String[] weekDay = new String[7]; 
@@ -35,18 +36,18 @@ public class SchemaActivity extends Activity {
         weekDay[1] = "Tue";
         weekDay[2] = "Wed";
         weekDay[3] = "Thu";
-        weekDay[4] = "Fri";
+        weekDay[4] = "Fri\t";
         weekDay[5] = "Sat";
         weekDay[6] = "Sun"; 
         
 
         // capture our View elements        
-        mTimeDisplay = (TextView) findViewById(R.id.timeDisplay);
-        mTimeDisplay1 = (TextView) findViewById(R.id.timeDisplay1);
-        mTimeDisplay2 = (TextView) findViewById(R.id.timeDisplay2);
-        mTimeDisplay3 = (TextView) findViewById(R.id.timeDisplay3);
-        mTimeDisplay4 = (TextView) findViewById(R.id.timeDisplay4);
-        mTimeDisplay5 = (TextView) findViewById(R.id.timeDisplay5);
+        mTimeDisplay[0] = (TextView) findViewById(R.id.timeDisplay);
+        mTimeDisplay[1] = (TextView) findViewById(R.id.timeDisplay1);
+        mTimeDisplay[2] = (TextView) findViewById(R.id.timeDisplay2);
+        mTimeDisplay[3] = (TextView) findViewById(R.id.timeDisplay3);
+        mTimeDisplay[4] = (TextView) findViewById(R.id.timeDisplay4);
+        mTimeDisplay[5] = (TextView) findViewById(R.id.timeDisplay5);
         mPickTime = (Button) findViewById(R.id.pickTime);
 
         // add a click listener to the button
@@ -63,9 +64,10 @@ public class SchemaActivity extends Activity {
 
         // display the current date
         updateDisplay();
-        //TODO fixa så att updateDisplay uppdaterar alla textfält (snyggt)
+        
         //TODO 24h-format i dialogen
         //TODO välj veckodag i dialogen
+        
     }
         
     
@@ -73,123 +75,37 @@ public class SchemaActivity extends Activity {
  // updates the time we display in the TextView
     private void updateDisplay() {
     	int day = 0, wakeDay = 0, wakeHour;
-    	    		
-    	if (mHour>15){
-    		wakeHour = mHour - 16;
-    		wakeDay++;
-    	}
-    	else 
-    		wakeHour = mHour + 8;
-    	mTimeDisplay.setText(
-    			new StringBuilder()
-    			.append(pad(mHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[day] +"\t\t\t\t"))
-				.append(pad(wakeHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[wakeDay])));  
     	
-    	mHour = mHour+4;
-    	day++;
-    	if (mHour>23){
-    		mHour = mHour-24;
-    		day++;
-    	}
-    	wakeDay = day;
-    	if (mHour>15){
-    		wakeHour = mHour - 16;
-    		wakeDay++;
-    	}
-    	else 
-    		wakeHour = mHour + 8;
-    	mTimeDisplay1.setText(
-    			new StringBuilder()
-    			.append(pad(mHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[day] +"\t\t\t\t"))
-				.append(pad(wakeHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[wakeDay]))); 
-    	
-    	mHour = mHour+4;
-    	day++;
-    	if (mHour>23){
-    		mHour = mHour-24;
-    		day++;
-    	}
-    	wakeDay = day;
-    	if (mHour>15){
-    		wakeHour = mHour - 16;
-    		wakeDay++;
-    	}
-    	else 
-    		wakeHour = mHour + 8;
-    	mTimeDisplay2.setText(
-    			new StringBuilder()
-    			.append(pad(mHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[day] +"\t\t\t\t"))
-				.append(pad(wakeHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[wakeDay])));   
-    	
-    	mHour = mHour+4;
-    	day++;
-    	if (mHour>23){
-    		mHour = mHour-24;
-    		day++;
-    	}
-    	wakeDay = day;
-    	if (mHour>15){
-    		wakeHour = mHour - 16;
-    		wakeDay++;
-    	}
-    	else 
-    		wakeHour = mHour + 8;
-    	mTimeDisplay3.setText(
-    			new StringBuilder()
-    			.append(pad(mHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[day] +"\t\t\t\t"))
-				.append(pad(wakeHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[wakeDay])));  	
-    	
-    	mHour = mHour+4;
-    	day++;
-    	if (mHour>23){
-    		mHour = mHour-24;
-    		day++;
-    	}
-    	wakeDay = day;
-    	if (mHour>15){
-    		wakeHour = mHour - 16;
-    		wakeDay++;
-    	}
-    	else 
-    		wakeHour = mHour + 8;
-    	mTimeDisplay4.setText(
-    			new StringBuilder()
-    			.append(pad(mHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[day] +"\t\t\t\t"))
-				.append(pad(wakeHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[wakeDay])));
-    	
-    	mHour = mHour+4;
-    	day++;
-    	if (mHour>23){
-    		mHour = mHour-24;
-    		day++;
-    		if (day>6)
-    			day = 0;
-    	}
-    	wakeDay = day;
-    	if (mHour>15){
-    		wakeHour = mHour - 16;
-    		wakeDay++;
+    	for (int i=0; i<6; i++){
+    		
+    		wakeDay = day;
+    		if (mHour>15){
+        		wakeHour = mHour - 16;
+        		wakeDay++;
+        	}
+        	else 
+        		wakeHour = mHour + 8;
     		if (wakeDay>6)
-    			wakeDay=0;
+    			wakeDay = 0;
+    		
+        	mTimeDisplay[i].setText(
+        			new StringBuilder()
+        			.append(pad(mHour)).append(":")
+    				.append(pad(mMinute)).append(("    " + weekDay[day] +"\t\t\t\t"))
+    				.append(pad(wakeHour)).append(":")
+    				.append(pad(mMinute)).append(("    " + weekDay[wakeDay])));  
+    		
+        	mHour = mHour+4;
+        	day++;
+        	if (mHour>23){
+        		mHour = mHour-24;
+        		day++;
+        	}
+        	
+        	
     	}
-    	else 
-    		wakeHour = mHour + 8;
-    	mTimeDisplay5.setText(
-    			new StringBuilder()
-    			.append(pad(mHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[day] +"\t\t\t\t"))
-				.append(pad(wakeHour)).append(":")
-				.append(pad(mMinute)).append(("    " + weekDay[wakeDay])));
+    	    		
+ 
     }
 
     private static String pad(int c) {
