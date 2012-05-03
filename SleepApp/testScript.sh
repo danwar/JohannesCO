166dfd18ad0416d28b8b0db613accb2be3b20244
Johannes Schygge <johannes.schygge@gmail.com>
2012-05-03 12:28:22 +0200

    Script Problem

--- SleepApp/testScript.sh
+++ SleepApp/testScript.sh
@@ -1,50 +1,132 @@
-#To activate the test, start a terminal and type the location of the test script. EX. "../Desktop/testScript.sh
-#
-#
 #!/bin/bash
+
+#To activate the test, start a cygwin terminal and type the location of 
+#the test script. EX. "/cygdrive/c/Users/Uzer/Desktop/testScript.sh
+
+#dos2unix.exe ./testScript.sh must be typed in before the script is being 
+#run
+
+#
 #
 # Requires git, ant and that the tools and platform-tools 
+
 # folder in the Android SDK is on your path.
+
 #
 # Retrieve the latest version from your repository?
+
 #
 
-TEST_DIR=$HOME/Desktop/tmp-build
-mkdir -p $TEST_DIR
+
+
+TEST_DIR=/cygdrive/c/Users/Uzer/Desktop/tmp-build
+mkdir -p 
+$TEST_DIR
+
+PATH=$PATH:/cygdrive/c/Program\ Files/Android/android-sdk/tools
+
 
 cd $TEST_DIR
+
 pwd
 
+
+
+get_git()
+{
+  
 rm -fr JohannesCO
 
+  
 git clone https://github.com/danwar/JohannesCO
+  
 RET=$?
+  
+
 if [ $RET -ne 0 ] 
+
 then
-   echo "Could not clone git repo...... $RET"
-   exit $RET
+ echo "Could not clone git repo...... $RET"
+    
+exit $RET 
+  
 fi
 
-#
-# Generate build.xml
-#
-
-cd JohannesCO
-RET=$?
-if [ $RET -ne 0 ] 
-then
-   echo "Could not go to JohannesCO ...... $RET"
-   exit $RET
-fi
+}
 
 
-android update project -p .
-android update test-project -m ../ -p test/
+
+
+
+
+
+prepare()
+{
+
+  
+# android update project -p .
+  
+# android update test-project -m ../ -p test/
+
+
+ls -al
+
+   
+
+android.bat update project -p .
+
+
+#android.bat update test -project -m ../ -p test/
+
+}
+
+
 
 #
 # Compile, sign with debug key install to emulator/device and
+
 # run tests.
+
 #
 
+#
+#  MAIN part of the script
+
+#
+
+
+#
+# Generate build.xml
+
+#
+
+
+
+cd JohannesCO
+
+cd SleepApp
+
+
+RET=$?
+
+if [ $RET -ne 0 ] 
+
+then
+   echo "Could not go to JohannesCO ...... $RET"
+   
+exit $RET
+
+fi
+
+
+
+
+get_git
+
+prepare
+	
+
 cd test
+
 ant run-tests
+
