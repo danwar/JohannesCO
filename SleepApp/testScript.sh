f9ea731229e7681348a15466e923bff690802139
unknown <Johannes@SilverSprint.(none)>
2012-05-09 15:44:16 +0200

    test och docs

--- SleepApp/testScript.sh
+++ SleepApp/testScript.sh
@@ -1,132 +1,64 @@
 #!/bin/bash
-
-#To activate the test, start a cygwin terminal and type the location of 
-#the test script. EX. "/cygdrive/c/Users/Uzer/Desktop/testScript.sh
-
-#dos2unix.exe ./testScript.sh must be typed in before the script is being 
-#run
-
 #
+#To activate the test, start a cygwin terminal and type the location of the 
+#test script. EX. # "cd /cygdrive/c/Users/Johannes/Desktop/"
+# A reformatter must be done: "dos2unix /cygdrive/c/Users/Johannes/Desktop/testScript.sh"
+# Finally: "/cygdrive/c/Users/Johannes/Desktop/testScript.sh" 
 #
 # Requires git, ant and that the tools and platform-tools 
-
 # folder in the Android SDK is on your path.
-
 #
 # Retrieve the latest version from your repository?
-
 #
 
+BASE=/cygdrive/c/Users/Johannes
 
 
-TEST_DIR=/cygdrive/c/Users/Uzer/Desktop/tmp-build
-mkdir -p 
-$TEST_DIR
-
-PATH=$PATH:/cygdrive/c/Program\ Files/Android/android-sdk/tools
-
+TEST_DIR=$BASE/Desktop/tmp-build
+mkdir -p $TEST_DIR
 
 cd $TEST_DIR
-
 pwd
 
-
-
-get_git()
-{
-  
 rm -fr JohannesCO
+echo "CLONE in $(pwd)"
 
-  
-git clone https://github.com/danwar/JohannesCO
-  
+'/cygdrive/c/Program Files (x86)/Git/bin/git' clone https://github.com/danwar/JohannesCO
+#/cygdrive/c/Program\ Files\ \(x86\)/Git/bin/git clone https://github.com/danwar/JohannesCO
 RET=$?
-  
-
 if [ $RET -ne 0 ] 
-
 then
- echo "Could not clone git repo...... $RET"
-    
-exit $RET 
-  
+   echo "Could not clone git repo...... $RET"
+   exit $RET
 fi
-
-}
-
-
-
-
-
-
-
-prepare()
-{
-
-  
-# android update project -p .
-  
-# android update test-project -m ../ -p test/
-
-
-ls -al
-
-   
-
-android.bat update project -p .
-
-
-#android.bat update test -project -m ../ -p test/
-
-}
-
-
-
-#
-# Compile, sign with debug key install to emulator/device and
-
-# run tests.
-
-#
-
-#
-#  MAIN part of the script
-
-#
-
 
 #
 # Generate build.xml
-
 #
 
-
-
 cd JohannesCO
-
-cd SleepApp
-
-
 RET=$?
-
 if [ $RET -ne 0 ] 
-
 then
    echo "Could not go to JohannesCO ...... $RET"
-   
-exit $RET
-
+   exit $RET
 fi
 
 
+# android update project -p .
+# android update test-project -m ../ -p test/
 
+#/cygdrive/c/Program\ Files/Android/android-sdk/tools/android.bat update project -p .
+#/cygdrive/c/Program\ Files/Android/android-sdk/tools/android.bat update test-project -m ../ -p test/
 
-get_git
+#
+# Compile, sign with debug key install to emulator/device and
+# run tests.
+#
 
-prepare
-	
-
+cd SleepApp
 cd test
 
-ant run-tests
-
+echo "WIll now execute ant..... I am in $(pwd)"
+sleep 2
+/cygdrive/c/Program\ Files/Ant/bin/ant.bat run-tests
