13a0f56df29d42584e9fac9eea6d16d7cd5b5818
Daniel Warme <daniel.warme@gmail.com>
2012-05-18 10:16:54 +0200

    test

---
+++ SleepApp/test/Converter.java
@@ -0,0 +1,22 @@
+
+
+public class Converter {
+    public int intToMillis(int hour, int min){
+   	 int timeMillis = (hour * 60 + min) * 60 * 1000;
+   	 return timeMillis;
+   }
+   
+   public int millisToHour(int Millis){
+   	int temp = Millis / (1000 * 60);
+   	int hour = (int) Math.floor(temp / 60);
+   	return hour;
+   }
+   
+   public int millisToMin(int Millis){
+   	int temp = Millis / (1000 * 60);
+   	int hour = (int) Math.floor(temp / 60);
+   	int min = temp - hour*60;
+   	return min;
+   }	
+}
+
